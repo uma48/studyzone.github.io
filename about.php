ece785366c687bfd338751d6b12e83ab187c8bfd
umasankar <umasankardash010@gmail.com>
2023-04-30 08:24:40 +0530

    my elearning website

---
+++ about.php
@@ -0,0 +1,221 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>We want you to study what you consider to be important. With our flexible course system we can make sure that our courses suit you perfectly. We can easily add or remove information in all our courses.</p>
+         <a href="courses.html" class="inline-btn">our courses</a>
+      </div>
+
+   </div>
+
+   <div class="box-container">
+
+      <div class="box">
+         <i class="fas fa-graduation-cap"></i>
+         <div>
+            <h3>+1k</h3>
+            <span>online courses</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-user-graduate"></i>
+         <div>
+            <h3>+25k</h3>
+            <span>brilliants students</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-chalkboard-user"></i>
+         <div>
+            <h3>+5k</h3>
+            <span>expert teachers</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-briefcase"></i>
+         <div>
+            <h3>100%</h3>
+            <span>job placement</span>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading">student's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <p>This platform is excellent for students as well as teachers. There are too many features which make learning easy. It gives a better connection between teacher and student.</p>
+         <div class="user">
+            <img src="images/pic-6.jpg" alt="">
+            <div>
+               <h3>Santosh Rout</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>It provides all facilities to crack any exam. I am very satisfied with EDUCATORS of this platform. The very important thing for students is interaction with educators not between learners.</p>
+         <div class="user">
+            <img src="images/pic-3.jpg" alt="">
+            <div>
+               <h3>Jyoti Mahapatra</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>This website is excellent. The way it represents the concept in the form of educational and attractive videos is just fabulous 😍.</p>
+         <div class="user">
+            <img src="images/pic-4.jpg" alt="">
+            <div>
+               <h3>Kailash ch Rout</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>This website is the most helpful one amongst all others, it's free; easy to use; contains very less ads; makes concepts fun and easy to learn.</p>
+         <div class="user">
+            <img src="images/pic-4.jpg" alt="">
+            <div>
+               <h3>Satyabadi sahoo</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>This website is excellent,I would definitely recommend it to anyone who is doubtful.</p>
+         <div class="user">
+            <img src="images/pic-6.jpg" alt="">
+            <div>
+               <h3>Soumya Dash</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="box">
+         <p>The website is great and the learning process and teachers are amazing. It helped me to understand each and every concept and even much more than the books.</p>
+         <div class="user">
+            <img src="images/pic-3.jpg" alt="">
+            <div>
+               <h3>Jyoti Panda</h3>
+               <div class="stars">
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star"></i>
+                  <i class="fas fa-star-half-alt"></i>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+
+   &copy; copyright @ <?= date('Y'); ?> by <span>mr.Dash & mr.Rout</span> | all rights reserved!
+
+</footer>
